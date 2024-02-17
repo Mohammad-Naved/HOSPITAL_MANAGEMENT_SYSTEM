8be65f3e66d974c61272fb3e65a6438f0134aca5
Mohammad-Naved <knaved782@gmail.com>
2024-02-17 17:48:32 +0530

    removed username and password

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -1,15 +1,14 @@
 package HospitalManagementSystem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.print.Doc;
+import java.sql.*;
 import java.util.Scanner;
 
 public class HospitalManagementSystem {
 
     private static final String url="jdbc:mysql://localhost:3306/hospital";
-    private static final String username="root";
-    private static final String password="root";
+    private static final String username="";//Provide the username of db
+    private static final String password="";//provide the password of db
 
     public static void main(String[] args) {
        /* try {
@@ -34,12 +33,89 @@ public class HospitalManagementSystem {
 
                 System.out.println("Enter your choice: ");
                 int choice=scanner.nextInt();
+                switch (choice){
+                    case 1:
+                        patient.addPatient();
+                        System.out.println();
+                        break;
+                    case 2:
+                        patient.viewPatient();
+                        System.out.println();
+                        break;
+                    case 3:
+                        doctor.viewDoctor();
+                        System.out.println();
+                        break;
+                    case 4:
+                       bookAppointment(patient,doctor,connection,scanner);
+                        System.out.println();
+                        break;
+                    case 5:
+                        System.out.println("THANK YOU FOR USING HOSPITAL MANAGEMENT SYSTEM");
+                        return;
+                    default:
+                        System.out.println("Entered valid choice!!!");
+                }
             }
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    public static void bookAppointment(Patient patient, Doctor doctor,Connection connection, Scanner scanner){
+        System.out.println("Enter Patient Id: ");
+        int patientId=scanner.nextInt();
+        System.out.println("Enter Doctor Id: ");
+        int doctorId=scanner.nextInt();
+        System.out.println("Enter appointment date (YYYY-MM-DD): " );
+        String appointmentDate=scanner.next();
+        if (patient.getPatientById(patientId) && doctor.getDoctorById(doctorId)){
+            if(checkDoctorAvailability(doctorId,appointmentDate,connection)){
+                String appointmentQuery="INSERT INTO appointments(patient_id, doctor_id, appointment_date) VALUES(?,?,?)";
+                try {
+                    PreparedStatement preparedStatement=connection.prepareStatement(appointmentQuery);
+                    preparedStatement.setInt(1,patientId);
+                    preparedStatement.setInt(2,doctorId);
+                    preparedStatement.setString(3,appointmentDate);
+                    int rowsAffected=preparedStatement.executeUpdate();
+                    if (rowsAffected>0){
+                        System.out.println("Appointment Booked!");
+                    }else{
+                        System.out.println("Failed toBook Appointment");
+                    }
+                } catch (SQLException e) {
+                   e.printStackTrace();
+                }
+            }else {
+                System.out.println("Doctor not available on this date!!");
+            }
+        }else {
+            System.out.println("Either doctor or patient doesn't exist!!!");
+        }
+
+    }
+
+    private static boolean checkDoctorAvailability(int doctorId, String appointmentDate,Connection connection) {
+        String query="SELECT COUNT(*) FROM appointments WHERE doctor_id = ? AND appointment_date = ?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            preparedStatement.setInt(1,doctorId);
+            preparedStatement.setString(2,appointmentDate);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                int count=resultSet.getInt(1);
+                if(count==0)
+                    return true;
+                else
+                    return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
 
     }
 
